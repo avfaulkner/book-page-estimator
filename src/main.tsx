e0b97108168ec4a55045177cb7bdaec8dd8e354f
afaulkner <avfaulkner@gmail.com>
2025-07-04 14:27:39 -0700

    add dependencies, file name fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from "./components/App";
-import { CoverProvider } from "./context/coverContext";
+import { CoverProvider } from "./context/CoverContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
